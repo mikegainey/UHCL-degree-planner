3bced17746caf891c77911a726e8631f8c17ca01
Michael Gainey <mikeg9b@gmail.com>
2017-10-24 18:59:58 -0500

    decided on a monolithic program
    - so other students can download and run one program (simplicity)
    - with data constant definitions
    - with function definitions (including main)
    - no code outside functions; uses if '__name__' == '__main__':
    - main file can be imported to a unittest file

--- UHCLdegreeplanner.py
+++ UHCLdegreeplanner.py
@@ -1,32 +1,25 @@
-# The UHCL undergraduate catalog can be found at the following URL:
-# https://www.uhcl.edu/academics/resources/catalog/documents/2016-2017-undergrad-catalog.pdf
+# Undergraduate Catalog 2017-2018, Computer Science B.S.
+# Degree Requirements: https://catalog.uhcl.edu/current/undergraduate/degrees-and-programs/bachelors/computer-science-bs
 
 # The 2-page Computer Science degreeplan can be found at:
 # https://www.uhcl.edu/academics/degrees/documents/cse/wbs-computerscience.pdf
 
-from degreeplan_data import * # import the course catalog, corereq, majorreq, and LLC
+#############
+# User data #
+#############
 
-coursestaken = {'MATH 2413', 'CSCI 1470'}
-
-# this doesn't include language, creative arts, and social science courses because there are options
-coursesneeded = corereq | majorreq # the intersection of corereq and majorreq
-coursesneeded = coursesneeded - coursestaken
-
-degreeplan = []
+coursestaken = set() # is this needed? options should be terminal or file input
 
 
-def prerequisites_met(course, coursestaken=coursestaken): # coursestaken could be removed because it is global
-    '''Given a proposed course and a list of courses taken, 
-       return True if the course's prerequisites have been met.
-       prerequisites_met(course : str, coursestaken : set) -> bool
-    '''
-    prerequisites = requirements[course][1]     # get the set of prerequisites for the course
-    return prerequisites.issubset(coursestaken) # is the set of prerequisites a subset of coursestaken?
-
+############################
+# Data used by the program #
+############################
 
 def isULC(course):
     '''Given a course return True if the course is an upper-level CSCI or CENG course.
        isULC(course : str) -> bool
+       Used to build the set constant ULC
+       * informally tested: seems to work
     '''
     isCSCI = course[:4] == 'CSCI'   # CSCI course?
     isCENG = course[:4] == 'CENG'   # CENG course?
@@ -34,33 +27,261 @@ def isULC(course):
     return (isCSCI or isCENG) and isULC
 
 
-def LLCcomplete(coursestaken, LLC=LLC):
+# coursecatalog is a dictionary
+#   key:   is a course rubric, 'PHYS 2325'
+#   value: is a tuple, consisting of
+#     - the full title of the course, "University Physics I"
+#     - a set of prerequisites, {"MATH 2413"}
+
+coursecatalog = {
+    # Communication (6 hours)
+    "WRIT 1301": ("Composition I", set()),
+    "WRIT 1302": ("Composition II", {"WRIT 1301"}),
+
+    # Mathematics (3 hours)
+    "MATH 2413": ("Calculus I", set()),
+
+    # Life and Physical Sciences (6 hours)
+    "PHYS 2325": ("University Physics I and Lab (PHYS 2125) (LLC)", {"MATH 2413"}),
+    "PHYS 2326": ("University Physics II and Lab (PHYS 2126) (LLC)", {"MATH 2414", "PHYS 2325"}),
+
+    # Language, Philosophy and Culture (3 hours)
+    "HUMN 1301": ("Humanities", set()),
+    "LITR 2341": ("Literature and Experience", {"WRIT 1301"}),
+    "PHIL 1301": ("Introduction to Philosophy", set()),
+    "WGST 1301": ("Gender Matters: Introduction to Women's and Gender Studies", set()),
+
+    # Creative Arts (3 Hours)
+    "ARTS 1303": ("World Art Survey I", set()),
+    "ARTS 1304": ("World Art Survey II", set()),
+    "ARTS 2379": ("Arts and the Child", set()),
+
+    # American History (6 hours)
+    "HIST 1301": ("U.S. History I", set()),
+    "HIST 1302": ("U.S. History II", set()),
+
+    # Government/Political Science (6 hours)
+    "POLS 2305": ("Federal Government", set()),
+    "POLS 2306": ("Texas Government", set()),
+    
+    # Social and Behavioral Sciences (3 hours)
+    "ANTH 2346": ("General Anthropology", set()),
+    "CRIM 1301": ("Introduction to Criminal Justice", set()),
+    "ECON 2301": ("Principles of Macroeconomics ", set()),
+    "ECON 2302": ("Principles of Microeconomics", set()),
+    "GEOG 1303": ("World Regional Geography", set()),
+    "PSYC 2301": ("Introduction to Psychology", set()),
+    "SOCI 1301": ("Introduction to Sociology", set()),
+
+    # Component Area Option (6 hours)
+    "COMM 1315": ("Public Speaking", set()),
+    "PSYC 1100": ("Learning Frameworks", set()),
+
+    # Major Requirements (67 hours)
+    "CHEM 1311": ("General Chemistry I and Lab (CHEM 1111)", set()),
+    "MATH 2305": ("Discrete Math (LLC)", {"MATH 2413"}),
+    "MATH 2318": ("Linear Algebra", {"MATH 2413"}),
+    "MATH 2414": ("Calculus II", {"MATH 2413"}),
+    "MATH 2320": ("Differential Equations", {"MATH 2414"}),
+    "STAT 3334": ("Probability & Statistics ...", {"MATH 2413", "MATH 2414"}),
+    "CSCI 1470": ("Computer Science I (LLC)", set()),
+    "CSCI 1471": ("Computer Science II (LLC)", {"CSCI 1470", "MATH 2413"}),
+    "CSCI 3331": ("Computer Organization & Assembly Language", {"CSCI 2315", "MATH 2305", "MATH 2414", 
+                                                                "PHYS 2325",  "PHYS 2326"}),
+    "CSCI 2315": ("Data Structures (LLC)", {"CSCI 1471"}),
+    "CSCI 3352": ("Advanced Data Structures", {"CSCI 2315", "MATH 2305", "MATH 2414", "PHYS 2325", "PHYS 2326"}),
+    "CSCI 4333": ("Design of Database Systems", {"CSCI 2315"}),
+    "CSCI 3321": ("Numerical Methods", {"MATH 2318", "MATH 2320", "CSCI 1471"}),
+    "CSCI 4354": ("Operating Systems (take with CENG 3351)", {"CSCI 2315", "CSCI 3331", "MATH 2305",
+                                                              "MATH 2414", "PHYS 2325", "PHYS 2326"}),
+
+    "CENG 3312": ("Digital Circuits & Lab (CENG 3112)", {"MATH 2414", "PHYS 2326"}),
+    "CENG 3331": ("Intro to Telecom and Neworks & Lab (CENG 3131)",  {"CENG 3312"}),
+    "CENG 3351": ("Computer Architecture & Lab (CENG 3151) (take with CSCI 4354)", {"CENG 3312"}),
+    
+    "SWEN 4342": ("Software Engineering", {"CSCI 1470", "CSCI 2315"}),
+    "WRIT 3315": ("Technical Writing", {"WRIT 1301", "WRIT 1302"}),
+    "CSCI 4388": ("Senior Project in Computer Science", {"CSCI 3352", "SWEN 4342"}),
+
+    "CSCI 4320": ("(elective) Web Application Development", {"CSCI 2315"}),
+    "CSCI 4350": ("(elective) Computer Graphics and Interface Design", {"MATH 2318",  "MATH 2413"}),
+    "CSCI 4362": ("(elective) Computer Game Programming: Theory and Practice", {"CSCI 1470"}),
+    "CSCI 4323": ("(elective) Computer Security", {"CSCI 1471"})}
+
+# Language, Philosophy and Culture (3 hours required)
+langPhilCulture = {"HUMN 1301", "LITR 2341", "PHIL 1301", "WGST 1301"}
+
+# Creative Arts (3 Hours)
+creativeArts = {"ARTS 1303", "ARTS 1304", "ARTS 2379"}
+
+# Social and Behavioral Sciences (3 hours required)
+socialScience = {"ANTH 2346", "CRIM 1301", "ECON 2301", "ECON 2302", "GEOG 1303", "PSYC 2301", "SOCI 1301"}
+
+
+corereq = {'WRIT 1301', 'WRIT 1302', 'MATH 2413', 'PHYS 2325', 'PHYS 2326', 'HIST 1301', 'HIST 1302',
+           'POLS 2305', 'POLS 2306', 'COMM 1315', 'PSYC 1100'}
+
+majorreq = {'CHEM 1311', 'MATH 2305', 'MATH 2318', 'MATH 2414', 'MATH 2320', 'STAT 3334',
+            'CSCI 1470', 'CSCI 1471', 'CSCI 3331', 'CSCI 2315', 'CSCI 3352', 'CSCI 4333', 'CSCI 3321',
+            'CSCI 4354', 'CENG 3312', 'CENG 3331', 'CENG 3351', 'SWEN 4342', 'WRIT 3315', 'CSCI 4388'}
+
+LLC = {'CSCI 1470', 'CSCI 1471', 'CSCI 2315', 'PHYS 2325', 'PHYS 2326', 'MATH 2413', 'MATH 2414', 'MATH 2305', 'WRIT 1301'}
+
+ULC = {c for c in majorreq if isULC(c)}
+
+CSelectives = set() # TODO: populate this set from the catalog
+
+
+#################################
+# Functions used by the program #
+#################################
+
+def prerequisites_met(course, coursestaken=coursestaken): # coursestaken could be removed because it is global
+    '''Given a proposed course and a list of courses taken, 
+       return True if the course's prerequisites have been met.
+       prerequisites_met(course : str, coursestaken : set) -> bool
+       * Informally tested: seems to work
+    '''
+    prerequisites = coursecatalog[course][1]     # get the set of prerequisites for the course
+
+    for p in prerequisites:       # check each prerequistite
+        if p not in coursestaken: # if a prerequisite is not in coursestaken
+            return False          # return False
+
+    return True                   # all prerequisites were in coursestaken
+
+
+def LLCcomplete(coursestaken=coursestaken, LLC=LLC):
     '''Given a set of coursestaken and the set of LLC, return True if the LLC is complete.
        LLCcomplete(coursestaken : set, LLC : set) -> bool
     '''
-    return LLC.issubset(coursestaken)
+    return LLC <= coursestaken # True if every element of LLC issubset of coursestaken
+
+
+def possibilities(coursestaken=coursestaken,
+                  corereq=corereq, majorreq=majorreq,CSelectives=CSelectives,
+                  langPhilCulture=langPhilCulture, creativeArts=creativeArts,
+                  socialScience=socialScience):
+
+    # at the end of this function, possibilities will be the list of courses eligible to be taken
+    # start with: corereq | majorreq | langPhilCulture | creativeArts | socialScience | CSelectives
+    possibilities = corereq | majorreq | langPhilCulture | creativeArts | socialScience | CSelectives
+
+    # remove coursestaken from possibilities
+    possibilities -= coursestaken
+    
+    # remove courses if prerequisitives have not been met
+    possibilities = {p for p in possibilities if prerequisites_met(p, coursestaken)}
+    print("After removing courses with unmet prerequisities:\n{}".format(possibilities))
+    
+    # remove ULC if LLC not complete
+    if not LLCcomplete:      # if LLC is not complete
+        possibilities -= ULC # remove ULC
+
+    # if langPhilCulture requirement met (3 hours), remove all langPhilCulture courses from possibilities
+    if len(langPhilCulture & coursestaken) > 0: # if the intersection of langPhilCulture and coursestaken is greater than zero
+        possibilities -= langPhilCulture        # remove all langPhilCulture courses from the possibilities list
+
+    # if creativeArts requirement met (3 hours), remove all creativeArts courses from possibilities
+    if len(creativeArts & coursestaken) > 0: # if the intersection of creativeArts and coursestaken is greater than zero
+        possibilities -= creativeArts        # remove all creativeArts courses from the possibilities list
+    
+    # if socialScience requirement met (3 hours), remove all socialScience courses from possibilities
+    if len(socialScience & coursestaken) > 0: # if the intersection of socialScience and coursestaken is greater than zero
+        possibilities -= socialScience        # remove all socialScience courses from the possibilities list
+
+    return possibilities
+
+
+def getCoursesTaken():
+    '''Prompt the user to enter courses previously completed.
+       getCoursesTaken() -> set'''
+
+    coursestaken = set() # start from an empty set
+    
+    print("\nList courses by rubric (like CSCI 1470) that you have successfully completed.  Press <Enter> when finished.\n")
+
+    while True:
+        course = input("  Enter a course rubric (like CSCI 1470): ")
+
+        if course == '':         # the sentinel
+            print()
+            return coursestaken # return coursestaken to main
+        
+        # validate input (improve this)
+        if len(course) != 9: 
+            print("Use the format: CSCI 1470")
+            continue
+
+        course = course.upper()
+        coursestaken.add(course)
+
+
+def getTerm():
+    '''Prompt the user to enter the starting term (like Fall 2017)
+       getTerm() -> str'''
+
+    seasons = ["placeholder", "Fall", "Spring", "Summer"]
+    print("\nEnter your starting term: 1) Fall  2) Spring or  3) Summer")
+    season = int(input("  Enter 1, 2, or 3: ")) # check the input first
+    season = seasons[season]
+    
+    year = input("\n  Enter your starting 2-digit year: ") # check the input
+
+    term = season + " 20" + year
+    return term
+
+
+def incTerm(term):
+    '''Given a term, return the next term
+       incTerm(term : str) -> str
+    '''
+    seasons = ["Fall", "Spring", "Summer"]
+    
+    season, year = term.split()
+
+    if season == 'Fall':
+        year = int(year) + 1
+        year = str(year)
+    
+    seasonx = seasons.index(season) # this will fail easily
+    nextseasonx = (seasonx + 1) % 3
+    nextseason = seasons[nextseasonx]
+
+    nextterm = nextseason + ' ' + year
+    return nextterm
+
+
+def displayChoices(term, courseSet):
+    '''Given an unordered set of course possibilities, display and return a sorted list
+       displayChoices(term : str, courseSet : set) -> [str]
+    '''
+
+    print("{}\n{} choices\n{}".format("=" * 40, term, "=" * 40))
+    
+    for i, c in enumerate(courseList):
+        print("{:2}) {} {}".format(i, c, coursecatalog[c][0]))
+
 
     
-# Print a list of courses than can be taken
-# - prerequisites met
-# - LLC complete (if needed)
-# - TODO: add language, creative arts, and social science courses
+def main():
 
-def listchoices(coursestaken, requirements=requirements, LLC=LLC):
-    '''Given the sets of coursestaken, requirements, and LLC, return a list of courses that can be taken
-       listchoices( coursestaken : set, requirements : set, LLC : set) -> [str]
-    '''
-    choices = coursesneeded
-    if not LLCcomplete(coursestaken, LLC):
-        # choices = choices - (upper level courses) # make a ULC constant
-    print(LLC)
-    for course in requirements:
-        p = prerequisites_met(course, coursestaken)
-        ul = isULC(course, requirements)
-        llc = LLCcomplete(coursestaken, LLC)
-        # print("course = {}, p = {:2}, UL = {:2}, LLC = {:2}".format(course, p, ul, llc))
-        if p and (not ul or llc):
-            choices.append(course)
-    return choices
+    term = getTerm() # let the user enter the starting term (like Fall 2017)
+    
+    coursestaken = getCoursesTaken() # let the user enter courses previously taken
+    
+    courseSet = possibilities(coursestaken) # a set of courses eligible to be taken
 
-listchoices(coursestaken)
+    courseList = displayChoices(term, courseSet) # display a sorted list of course choices
+
+    print()
+    
+    # while True:
+    #     choice = input("Select a course or press <Enter> to continue: ")
+        
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
