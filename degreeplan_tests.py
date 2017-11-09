bdc27c877650fd5e8b835cefaa8dd6b6df2ac8d5
Michael Gainey <mikeg9b@gmail.com>
2017-11-08 19:59:21 -0600

    tested isRubric

--- degreeplan_tests.py
+++ degreeplan_tests.py
@@ -103,12 +103,57 @@ class TestFunctions(unittest.TestCase):
                         'MATH 2413', 'MATH 2414', 'MATH 2305', 'WRIT 1301'}
         choices = UNI_CORE | MAJOR_REQ | LANG_PHIL_CULTURE | CREATIVE_ARTS | SOCIAL_SCIENCE | ELECTIVES
         choices = {course for course in choices if prerequisites_met(course, coursestaken)}
-        choices -= LLC # remove LLC from choices
+        choices -= coursestaken # remove courses taken from choices
         self.assertEqual(getChoices(coursestaken), choices)
 
-        # I should probably test the set comprehension that removes courses with unmet prerequisites, but
-        # it isn't a function.  It's just one line of Python.
-       
+        # The next test tests the set comprehension: choices = {course for course in choices if ...}
+        
+        
+    def test_prereq_comprehension(self):
+        '''This tests the one-liner set comprehension that takes a set of coureses and filters out courses
+           with unmet prerequisites:
+           choices = {course for course in choices if prerequisites_met(course, coursestaken)}
+        '''
+        coursestaken = set() # only coureses with no prerequisites can be taken
+        choices = {'CSCI 1470', 'CSCI 1471'} # CSCI 1471 has prerequisites (CSCI 1470 and MATH 2413)
+        choices = {course for course in choices if prerequisites_met(course, coursestaken)}
+        self.assertEqual(choices, {'CSCI 1470'}) # only CSCI 1470 should make it through
+
+        coursestaken = {'CSCI 1470', 'MATH 2413'}
+        choices = {'CSCI 1471', 'CSCI 2315'} # CSCI 1471 prereqs are met; CSCI 2315 prereqs NOT met
+        choices = {course for course in choices if prerequisites_met(course, coursestaken)}
+        self.assertEqual(choices, {'CSCI 1471'}) # only CSCI 1471 should make it through
+
+
+    def test_isRubric(self):
+        '''Given a string, return True if the string consists of 4 alpha + ' ' + 4 decimal characters
+           isRubric(maybeRubric : str) -> bool
+           the alpha characters can be uppercase or lowercase
+        '''
+        self.assertFalse(isRubric('CompSci 1470')) # length > 9 characters
+        self.assertFalse(isRubric('CSCI 147')) # length < 9 characters
+        self.assertFalse(isRubric('CSCI_1470')) # only 1 word
+        self.assertFalse(isRubric('CS 1 1470')) # 3 words
+        self.assertFalse(isRubric('CSC1 1470')) # first word is not all alphabetic
+        self.assertFalse(isRubric('CSCI 1A70')) # second word is not all decimal
+        self.assertFalse(isRubric('CSC 147  ')) # first and second words not of length 4
+
+        # checking that a course applies to degree requirements is performed in add2CoursesTaken
+        self.assertTrue(isRubric('asdf 1234')) # conforms to the correct format; 
+
+
+#     # first word should be 4 alphabetic characters: CSCI
+#     part1 = words[0]
+#     if not (part1.isalpha() and len(part1) == 4): 
+#         return False
+
+#     # second word should be 4 decimal characters: 1470
+#     part2 = words[1]
+#     if not (part2.isdecimal() and len(part2) == 4): 
+#         return False
+
+#     return True # return True if above conditions are met
+
         
 
 
